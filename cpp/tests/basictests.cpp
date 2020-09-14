1c91ecb7b005a7fa4eaa44311607805512bd202d
imp4l3r <sogtrhs@gmail.com>
2020-09-14 20:38:48 +0200

    fixed googletest+corrections to cmakefile

--- cpp/tests/basictests.cpp
+++ cpp/tests/basictests.cpp
@@ -1,12 +1,13 @@
 #include <gtest/gtest.h>
 #include <stdexcept>
+#include <string>
 
 class BasicTest : public ::testing::Test {
     protected:
         void SetUp() override {}
 
         void TearDown() override {}
-}
+};
 
 TEST_F(BasicTest, BasicExample) {
     ASSERT_EQ(1,1);
