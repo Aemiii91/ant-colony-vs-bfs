c7d5238c71c198d97b92606c53a56cb20082de2b
imp4l3r <sogtrhs@gmail.com>
2020-09-14 17:54:12 +0200

    added googletest

---
+++ cpp/tests/basictests.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include <stdexcept>
+
+class BasicTest : public ::testing::Test {
+    protected:
+        void SetUp() override {}
+
+        void TearDown() override {}
+}
+
+TEST_F(BasicTest, BasicExample) {
+    ASSERT_EQ(1,1);
+}
