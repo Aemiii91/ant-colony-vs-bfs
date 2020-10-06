a02a7659e1ecee178f80a3e50520f9f0b8e8e3d2
mholdg16 <mholdg16@student.aau.dk>
2020-10-06 13:58:25 +0200

    Added geocaching api

--- py/main.py
+++ py/main.py
@@ -1,9 +1,11 @@
 import openroute
+from geocaching import geocaching
 
-def main():
+def __main__():
   print("This is main")
+  results = geocaching.search(57.0,9.0)
+  print(results)
   openroute.importMatrix()
 
-
 if __name__ == "__main__":
-   main() 
+   __main__() 
