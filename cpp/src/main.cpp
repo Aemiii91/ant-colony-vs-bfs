0c7d29c909260ff3f26b84d4944130ea4f1da839
Andrikz <andershb1234@gmail.com>
2020-10-08 13:52:28 +0200

    changed to use vec in claas

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 #include "test.h"
 #include "poi.h"
-#include "graph/wGraph.h"
-using namespace std;
+#include "boostGraph/wGraph.h"
+#include <boost/graph/depth_first_search.hpp>
 
+
+using namespace std;
 int main (int argc, char **argv) {
-    std::cout << "testing opening of filestream" << std::endl;
-    
-   
-    vector<nodeData> nodelist;
-    vector<edgeData> edgeList;
+    std::cout << "testing opening of filestream" << std::endl; 
+    wGraph graph;
     for (int i = 0; i < 10; ++i) {
         nodeData temp("1");
-        nodelist.push_back(temp);
-        edgeData ed(i, i, random());
-        edgeList.push_back(ed);
-    }
-    
-    wGraph graph;
-    graph.populateGraph(nodelist, edgeList);  
+        graph.nodeList.push_back(temp);
+        edgeData ed(i, (rand() % 9), 12 );
+        graph.edgeList.push_back(ed);
+    }  
+    graph.populateGraph();  
+
 
     PoiImport imp;
     fstream fileStream;
@@ -26,15 +24,15 @@ int main (int argc, char **argv) {
 
     fileStream.open(filepath);
     
-   // if(fileStream.is_open()) {
-   //     std::cout << "filestream is good, importing dataset" << std::endl;
-   //     imp.ImportPoi(&fileStream);
-   //     std::cout << "imported poi count: " << imp.importedPoi.size() << std::endl;
-   // }
-   // else {
+    if(fileStream.is_open()) {
+        std::cout << "filestream is good, importing dataset" << std::endl;
+        imp.ImportPoi(&fileStream);
+        std::cout << "imported poi count: " << imp.importedPoi.size() << std::endl;
+    }
+    else {
 
-   //     std::cout << "Something went wrong with opening the file" << std::endl;
-   // }
-   //  
-   // return 0;
+        std::cout << "Something went wrong with opening the file" << std::endl;
+    }
+     
+    return 0;
 }
