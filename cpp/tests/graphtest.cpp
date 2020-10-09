50c039a8cbff52ef13e44908c9d58a9cd5147281
Andrikz <andershb1234@gmail.com>
2020-10-09 20:40:13 +0200

    Test things work, hint they do

--- cpp/tests/graphtest.cpp
+++ cpp/tests/graphtest.cpp
@@ -28,8 +28,9 @@ TEST(GraphTest, addEdge) {
   graph.addNode(n);
   graph.addNode(n2);
 
-  Edge e(1.2, n.ID);
-  graph.direcAddEdge(n2, e);
+  Edge e(n.ID, n2.ID, 1.2);
 
-  ASSERT_EQ(n.ID, graph.nodelist.at(n2.ID).edgeList.front().dist);
+  ASSERT_EQ(n.ID, graph.nodelist.at(n2.ID).edgeList.front().scr);
+  ASSERT_EQ(1.2, graph.nodelist.at(n2.ID).edgeList.front().weight);
+  ASSERT_EQ(n2.ID, graph.nodelist.at(n2.ID).edgeList.front().dist);
 }
