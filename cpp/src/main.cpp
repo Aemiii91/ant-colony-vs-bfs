7a35ac36ccd1d3957b549b3e3767e792a6fde55a
Simon Li <45622274+MadeYouLookPrank@users.noreply.github.com>
2020-11-11 02:35:14 +0100

    Second draft algorithm based on DFS approach (#17)
    * Added bool variable in node
    
    * Comment for adding bool to Node structure
    
    * Code reformatted
    
    * Added implementation of second draft algorithm using DFS
    
    * Fixed so it returns _path from bestPath
    
    * Added childrensChecked = true in GetSecondBestNode function
    
    * Added test cases to seconddraftalgorithmtest
    
    * ALGORITHM SEGFAULTS CURRENT; put stuff into the other test file
    
    * Added edges to test graph
    
    * Test changes for Second Draft
    
    * First test works
    
    * Fixed second test
    
    * Fixes in alg
    
    * Final debug
    
    * Bugfix for test
    
    * added path back to start node
    
    * Added path back to root node for every path calculations
    
    * Documentation for the method in the second draft algorithm.
    
    * Removed print statements for debugging
    
    * Added print for testing again because it helps alot
    
    * Added counter limit for testing
    
    * Last push but formatted
    
    Co-authored-by: Shizukanawa <deviousfinest@gmail.com>

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,4 +1,6 @@
 #include "jsonparser.h"
+#include "ownAlgorithm/baseAlgorithm.h"
+#include "ownAlgorithm/depthFirstAlgorithm.h"
 #include "ownAlgorithm/naiveAlgorithm.h"
 #include <ctime>
 #include <iostream>
@@ -7,5 +9,27 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-	return 0;
+	JsonParser parser;
+	Graph graph = parser.ParseData("matrix500.json");
+	int interval = 50000;
+
+	DFSAlgorithm Alg(interval, graph);
+	vector<Node> resAlg = Alg.SecondDraft();
+
+	NaiveAlgorithm Naive(interval, graph);
+	vector<Node> resNaive = Naive.FirstDraftAlgo();
+	cout << endl;
+	cout << endl;
+
+	cout << "Interval: " << interval << endl;
+	cout << "Graph: matrix500.json" << endl;
+	cout << endl;
+
+	cout << "NaiveAlgorithm: RESULTS" << endl;
+	Naive.PathPrinter();
+	cout << endl;
+
+	cout << "DFSAlgorithm: RESULTS" << endl;
+	Alg.PathPrinter();
+	cout << endl;
 }
