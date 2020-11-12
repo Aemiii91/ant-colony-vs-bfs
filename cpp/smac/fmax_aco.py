0aa01de2099bbc57669e25d1eae0d0b6b5b7302d
imp4l3r <sogtrhs@gmail.com>
2020-11-12 08:38:48 +0100

    more parameters

--- cpp/smac/fmax_aco.py
+++ cpp/smac/fmax_aco.py
@@ -1,32 +1,37 @@
 """
 Smac scripts for running aco, very minimal working example only varying one variable
 """
-
+ 
 import logging
 import subprocess
 import os
-
+ 
 from smac.facade.func_facade import fmin_smac
-
+ 
 def acorunner(x):
     upperScore = 100
-    staticparams = 'aco --data "matrix500.json" --colonies 5 --ants 50 --iterations 100 --alpha '
-    dynamicAlpha = x[0]
+    staticparams = 'aco --data "matrix500.json" --colonies 8 --ants 20 --iterations 40 '
+    dynamicAlpha = '--alpha ' + str(x[0])
+    dynamicBeta  = '--beta ' + str(x[1])
+    dynamicEvaporation = '--evaporation ' + str(x[2]) 
+    dynamicPheromone = '--pheromone ' + str(x[3])
+    dynamicParams = dynamicAlpha + dynamicBeta + dynamicEvaporation + dynamicPheromone
     cmd = "./routeplanner "
-    stdoutdata = subprocess.getoutput(cmd + staticparams + str(dynamicAlpha))
-    print("dakke dak dakkity dakk: " + stdoutdata)
+    stdoutdata = subprocess.getoutput(cmd + staticparams + dynamicParams)
+    print('stdoutdata := ' + stdoutdata)
     return upperScore - int(stdoutdata)
-
-
+ 
+ 
 # debug output
 logging.basicConfig(level=20)
 logger = logging.getLogger("Optimizer")  # Enable to show Debug outputs
-
-
-
+ 
+# cost (time constraint) is set as a default in the program as 30k
+# arguments in x in order: alpha, beta, evaporationRate, pheromone
 x, cost, _ = fmin_smac(acorunner,
-        x0=[1], #default config
-        bounds=[(0,50)], #limits
-        maxfun=30, #max number of evaluations
+        x0=[1, 20, 0.4, 100], #default config
+        bounds=[(0,20), (5,25), (0.01, 0.99), (50,100)], #limits
+        maxfun=500, #max number of evaluations
         rng=3) #random seed
 print("Best x: %s; with cost: %f" % (str(x), cost))
+
