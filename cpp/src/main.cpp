397697355d0b1b9670b1e68cca7dbccccd9db371
imp4l3r <sogtrhs@gmail.com>
2020-12-08 18:04:21 +0100

    changed main to fit aco

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -18,18 +18,51 @@
 #include <sstream>
 
 int main(int argc, char **argv) {
-	JsonParser parser;
+/*	JsonParser parser;
 	Graph graph = parser.ParseData(argv[1]);
 	int k = std::stoi(argv[2]);
 	int time = std::stoi(argv[3]);
 	int start = std::stoi(argv[4]);
-	DFSBranchLimitAlgorithm wow(30000,graph,time,start);
+	DFSBranchLimitAlgorithm wow(30000, graph, time, start);
 	vector<Node> reswow = wow.FourthDraft(k);
 	vector<int> result;
-	for(auto &iterator : reswow) {
+	for (auto &iterator : reswow) {
 		result.push_back(iterator.ID);
 	}
-	aco::Solution solution(wow._pathCost,result);
-	std::cout << solution;
-	return 0;
+	aco::Solution solution(wow._pathCost, result);
+	std::cout << solution; */
+
+    utils::ArgumentParser args(argc, argv);
+
+    // enable/disable output colors
+    termcolor::setEnabled(args.Exists("--colors"));
+
+    // default dataset path
+    std::string path = "../data/matrix500.json";
+    // get inputted dataset path
+    args.Get("--data", &path);
+
+    // load the graph
+    JsonParser parser;
+    Graph graph = parser.ParseData(path);
+
+    if (argc <= 1) {
+        std::cout << "No subprogram specified." << std::endl;
+        return 0;
+    }
+
+    // first argument represents the name of a subprogram
+    std::string subprogram = argv[1];
+
+    if (subprogram == "aco") {
+        aco::AntColony::run(&graph, &args);
+    } else if (subprogram == "naive") {
+        // run naive algorithm
+        std::cout << "Not implemented." << std::endl;
+    } else {
+        std::cout << termcolor::red << "Error: " << termcolor::reset;
+        std::cout << "Subprogram not recognized." << std::endl;
+    }
+
+    return 0;
 }
