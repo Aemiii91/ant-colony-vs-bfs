5017bf8f5da4a7c155b89850d927f06979d7534f
imp4l3r <sogtrhs@gmail>
2020-12-10 10:35:34 +0100

    average score

--- cpp/smac/godrun/getaveragescore.py
+++ cpp/smac/godrun/getaveragescore.py
@@ -2,6 +2,11 @@ import subprocess
 import sys
 from getscore import getScore
 
+def AppendtoFile(string):
+    resstring = str(string[0]) + "," + str(string[1]) + "\n"
+    with open("averagegodrunres.txt","a") as out:
+              out.write(resstring)
+
 def calculateAverage(argv):
     args = argv
     if args:
@@ -10,7 +15,7 @@ def calculateAverage(argv):
         print("Please specify a json file.")
         return
     
-    cmd = './routeplanner aco --iterations 500000 --data "' + str(args) + '" --time '
+    cmd = './routeplanner aco --iterations 500000 --data "' + str(args) + '" --alpha 65 --beta 9 --ants 971 --evaporation 0.0353 --time '
     print(cmd)
     accumulatedAverage = 0
     calculatedAverage = 0
@@ -19,7 +24,10 @@ def calculateAverage(argv):
     for x in times:
         for y in range(iterations):
             stdoutdata = subprocess.getoutput(cmd + str(x))
-            accumulatedAverage += getScore(stdoutdata)
+            score = getScore(stdoutdata)
+            paramstring = [x,score]
+            AppendtoFile(paramstring)
+            accumulatedAverage += score
             print("Iteration: " + str(y) + " acumulatedAverage: " + str(accumulatedAverage))
                 
         calculatedAverage = accumulatedAverage / iterations
@@ -27,4 +35,8 @@ def calculateAverage(argv):
         accumulatedAverage = 0
 
 if __name__ == '__main__':
+    with open("averagegodrunres.txt","a") as out:
+        out.write("time,score\n")
     calculateAverage(sys.argv[1])
+    # write header
+    
