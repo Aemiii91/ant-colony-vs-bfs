fd11a21b0496cd62f2680a50dae1288ad614ff50
imp4l3r <sogtrhs@gmail.com>
2020-12-15 14:26:03 +0100

    stronger godrun, albeit on wrong branch

---
+++ cpp/smac/stronger_godrun/run_berlin.py
@@ -0,0 +1,12 @@
+from IStrongwrapper import IAcoWrapper as acow
+
+def runBerlin(x):
+    cmd1 = './routeplanner aco --data "berlin.json" --iterations 500000 --progress2 --time 1 '
+    cmd10 = './routeplanner aco --data "berlin.json" --iterations 500000 --progress2 --time 10 '
+    cmd30 = './routeplanner aco --data "berlin.json" --iterations 500000 --progress2 --time 30 '
+    cmd60 = './routeplanner aco --data "berlin.json" --iterations 500000 --progress2 --time 60 '
+    res1 = acow(cmd1,x,60)
+    res10 = acow(cmd10,x,60)
+    res30 = acow(cmd30,x,60)
+    res60 = acow(cmd60,x,60)
+    return (res1 + res10 + res30 + res60)
